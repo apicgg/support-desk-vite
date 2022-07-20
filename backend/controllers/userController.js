6b0cc4913e7d04a23ce8a78a3ddff9f2a8eada80
Anurag Pramanik <p.anurag@outlook.com>
2022-07-21 00:56:08 +0530

    Add error middleware

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,7 +1,15 @@
+// Register users
 const registerUser = (req, res) => {
-  res.send('Register Route')
+  const { name, email, password } = req.body
+
+  // Validation
+  if (!name || !email || !password) {
+    res.status(400)
+    throw new Error('Please include all the fields')
+  }
 }
 
+// Login users
 const loginUser = (req, res) => {
   res.send('Login Route')
 }
--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,13 @@
 const express = require('express')
 const dotenv = require('dotenv').config()
+const { errorHandler } = require('./middleware/errorMiddleware')
 const PORT = process.env.PORT || 5000
 
 const app = express()
+
+// To handle the url encoded incoming data via api
+app.use(express.json())
+app.use(express.urlencoded({ extended: false }))
 
 app.get('/', (req, res) => {
   res.status(200).json({ message: `Welcome to Support Desk API` })
@@ -10,5 +15,8 @@ app.get('/', (req, res) => {
 
 // Routes
 app.use('/api/users', require('./routes/userRoute'))
+
+// Error middleware
+app.use(errorHandler)
 
 app.listen(PORT, () => console.log(`Server started on ${PORT}`))
