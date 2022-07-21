5c16d9900c2fa0142b1b9196a9ccafe411a48890
Anurag Pramanik <p.anurag@outlook.com>
2022-07-22 00:44:13 +0530

    Create login api and install jwt

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -50,7 +50,21 @@ const registerUser = asyncHandler(async (req, res) => {
 // /api/users/login
 // @public access
 const loginUser = asyncHandler(async (req, res) => {
-  res.send('Login Route')
+  const { email, password } = req.body
+
+  const user = await User.findOne({ email })
+
+  // Check user and password
+  if (user && (await bcrypt.compare(password, user.password))) {
+    res.status(200).json({
+      _id: user._id,
+      name: user.name,
+      email: user.email,
+    })
+  } else {
+    res.status(401)
+    throw new Error('Invalid credentials')
+  }
 })
 
 module.exports = {
