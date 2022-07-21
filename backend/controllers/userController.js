e3079006d21201a7697d09e9ee2a3452266b573a
Anurag Pramanik <p.anurag@outlook.com>
2022-07-21 22:00:47 +0530

    Add bcrypt and user schema

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,6 +1,11 @@
 const asyncHandler = require('express-async-handler')
+const bcrypt = require('bcryptjs')
 
-// Register users
+const User = require('../models/userModel')
+
+// Register a new user
+// /api/users
+// @public access
 const registerUser = asyncHandler(async (req, res) => {
   const { name, email, password } = req.body
 
@@ -9,9 +14,41 @@ const registerUser = asyncHandler(async (req, res) => {
     res.status(400)
     throw new Error('Please include all the fields')
   }
+
+  // Find if the user already exists
+  const userExist = await User.findOne({ email })
+
+  if (userExist) {
+    res.status(400)
+    throw new Error('User already exists')
+  }
+
+  // Hash the password
+  const salt = await bcrypt.genSalt(10)
+  const hashedPassword = await bcrypt.hash(password, salt)
+
+  // Create user
+  const user = await User.create({
+    name,
+    email,
+    password: hashedPassword,
+  })
+
+  if (user) {
+    res.status(201).json({
+      _id: user._id,
+      name: user.name,
+      email: user.email,
+    })
+  } else {
+    res.status(400)
+    throw new Error('Invalid user data')
+  }
 })
 
 // Login users
+// /api/users/login
+// @public access
 const loginUser = asyncHandler(async (req, res) => {
   res.send('Login Route')
 })
