b559a4c0b7ec038f16b1f9ce7d800d2b43ec101a
Anurag Pramanik <p.anurag@outlook.com>
2023-01-06 10:52:13 +0530

    fix(port): change the server port to 8000

--- backend/server.js
+++ backend/server.js
@@ -1,28 +1,28 @@
-const express = require('express')
-const dotenv = require('dotenv').config()
-const { errorHandler } = require('./middleware/errorMiddleware')
-const colors = require('colors')
-const connectDB = require('./config/db')
-const PORT = process.env.PORT || 5000
+const express = require("express");
+const dotenv = require("dotenv").config();
+const { errorHandler } = require("./middleware/errorMiddleware");
+const colors = require("colors");
+const connectDB = require("./config/db");
+const PORT = process.env.PORT || 8000;
 
 // Connect to MongoDB
-connectDB()
+connectDB();
 
-const app = express()
+const app = express();
 
 // To handle the url encoded incoming data via api
-app.use(express.json())
-app.use(express.urlencoded({ extended: false }))
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
 
-app.get('/', (req, res) => {
-  res.status(200).json({ message: `Welcome to Support Desk API` })
-})
+app.get("/", (req, res) => {
+  res.status(200).json({ message: `Welcome to Support Desk API` });
+});
 
 // Base Route
-app.use('/api/users', require('./routes/userRoute'))
-app.use('/api/tickets', require('./routes/ticketRoute'))
+app.use("/api/users", require("./routes/userRoute"));
+app.use("/api/tickets", require("./routes/ticketRoute"));
 
 // Error middleware
-app.use(errorHandler)
+app.use(errorHandler);
 
-app.listen(PORT, () => console.log(`Server started on ${PORT}`))
+app.listen(PORT, () => console.log(`Server started on ${PORT}`));
